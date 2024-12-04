87037ca8339d96e54da310f2d3ba801859565588
Bry <155580880+BCODERW@users.noreply.github.com>
2024-12-04 18:28:54 -0500

    Create wordToBrainFuck.py
    Put a word in the String word, and it gets converted to brainFuck code compatible with my brainFuck compiler

---
+++ wordToBrainFuck.py
@@ -0,0 +1,19 @@
+letters = [' ','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9','\'',',','.','!','?','+','-','[',']','>','<']
+word = list('+++++++++[->+++++++++<]>[-<+>]')
+elements = []
+for i in range(len(word)):
+    elements.append(letters.index(word[i]))
+print(elements)
+elements2 = []
+for i in range(len(elements)):
+    for z in range(elements[i]):
+        elements2.append('+')
+    elements2.append('.')
+    elements2.append('[')
+    elements2.append('-')
+    elements2.append(']')
+print(elements2)
+elementsString = ''
+for i in range(len(elements2)):
+    elementsString = elementsString + str(elements2[i])
+print(elementsString)    
